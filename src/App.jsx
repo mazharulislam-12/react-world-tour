4e166acdb287c6077dd17bafd9d7b54eeaf48b7d
mazharulislam-12 <505mazharulislam@gmail.com>
2023-09-09 20:29:32 -0700

    data load

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Countries from './components/Countries/Countries'
 
 function App() {
   
@@ -7,7 +8,7 @@ function App() {
     <>
       
       <h1> React World Tour</h1>
-     
+     <Countries></Countries>
 
      
     </>
